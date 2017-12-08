37b00eefc499266cad6ba63d79184e09eac97426
Adam Dueck <adueck@users.noreply.github.com>
2017-12-08 22:59:14 +0400

    Update split-video-by-srt.sh

--- split-video-by-srt.sh
+++ split-video-by-srt.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-read -e -p "Enter name of video file to split into chunks: " fileToCut 
+read -e -p "Enter name of video file to split into chunks: " fileToCut
 read -e -p "Enter name of subtitle file: " subtitleFile
 fileName=$(basename "$fileToCut")
 fileExt="${fileName##*.}"
 fileName="${fileName%.*}"
 
-if [ -f "$subtitleFile" ] 
+if [ -f "$subtitleFile" ]
 then
-  i=1
-  egrep "[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}" "$subtitleFile" | while read -r line ; do
+  i=0
+  echo "Extracting timecodes from subtitle file..."
+  while read -r line ; do
    startTime=`echo $line | egrep -o "^[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}"`
    endTime=`echo $line | egrep -o " [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}"`
-   startTimeForFfmpeg=`echo $startTime | sed 's/,/./'`
-   echo "Cut no $i"
+   startTimeForFfmpeg[i]=`echo $startTime | sed 's/,/./'`
    startDate=$(date -u -d "$startTime" +"%s.%N")
    endDate=$(date -u -d "$endTime" +"%s.%N")
-   timeDiff=$(date -u -d "0 $endDate sec - $startDate sec" +"%H:%M:%S.%N")
-   echo "Will start at $startTimeForFfmpeg and go $timeDiff"
-   ffmpeg -i $fileToCut -ss $startTimeForFfmpeg -t $timeDiff -async 1 -strict -2 $fileName${i}.$fileExt
+   timeDiff[i]=$(date -u -d "0 $endDate sec - $startDate sec" +"%H:%M:%S.%N")
    i=$[i+1]
-   read -p "for some reason I need to have this here" yes
-  done 
+  done < <(egrep "[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}" "$subtitleFile")
 else
   echo "no file found there";
 fi
+echo "done"
+
+arrayLength=${#startTimeForFfmpeg[@]}
+
+for (( j=0; j<${arrayLength}; j++));
+do
+  k=`expr $j + 1`
+  ffmpeg -i $fileToCut -ss "${startTimeForFfmpeg[j]}" -t "${timeDiff[j]}" -strict -2 $fileName${k}.$fileExt
+
+done
